117ad838e8bae9d86b8a0cceefa49add892eb192
Roufan Liau <roufan@gmail.com>
2022-03-04 20:44:01 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:lrcokl/ui/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lee Rubber Chinese Orchestra 吉隆坡南益校友会华乐团',
+      theme: ThemeData(
+        primarySwatch: white,
+      ),
+      home: Home(),
+      // debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+const MaterialColor white = MaterialColor(
+  0xFFFFFFFF,
+  <int, Color>{
+    50: Color(0xFFFFFFFF),
+    100: Color(0xFFFFFFFF),
+    200: Color(0xFFFFFFFF),
+    300: Color(0xFFFFFFFF),
+    400: Color(0xFFFFFFFF),
+    500: Color(0xFFFFFFFF),
+    600: Color(0xFFFFFFFF),
+    700: Color(0xFFFFFFFF),
+    800: Color(0xFFFFFFFF),
+    900: Color(0xFFFFFFFF),
+  },
+);
---
+++ lib/ui/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+
+class Home extends StatelessWidget {
+  Home({
+    Key? key,
+  }) : super(key: key);
+
+  String html = '''
+           <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Flrcokl%2Fvideos%2F283138963839493%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+     ''';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 120,
+        backgroundColor: Colors.white,
+        centerTitle: false,
+        title: Image.asset(
+          'assets/logos/main.jpg',
+          height: 110,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.menu),
+            tooltip: '',
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: HtmlWidget(
+          html,
+          webView: true,
+        ),
+      ),
+    );
+  }
+}
